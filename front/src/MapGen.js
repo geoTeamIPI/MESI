74877061fd8b4d49d5a4d83301587a0398f8795c
nicolas.hel <nicolas.hel@cgi.com>
2018-04-23 16:46:10 +0200

    map now working

--- front/src/MapGen.js
+++ front/src/MapGen.js
@@ -8,14 +8,10 @@ export default class MapGen extends Component {
     render() {
         const provider = new OpenStreetMapProvider();
 
-        // const searchControl = new GeoSearchControl({
-        //     provider: provider,
-        // });
-
-        const map = new L.Map('mapGen', {
-            center: [51.505, -0.09],
-            zoom: 13
-        });
+        const map = new L.Map('mapGen').setView([51.505, -0.09], 13);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
 
         new GeoSearchControl({

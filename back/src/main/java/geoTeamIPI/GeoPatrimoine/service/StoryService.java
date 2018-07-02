2be1ceeb562e59068df92fa14f3a82514d4dc63e
rousslud <rousselot.ludovic@yahoo.fr>
2018-07-02 09:59:48 +0200

    [BACK] Stories CRUD + count + listing

--- back/src/main/java/geoTeamIPI/GeoPatrimoine/service/StoryService.java
+++ back/src/main/java/geoTeamIPI/GeoPatrimoine/service/StoryService.java
@@ -7,76 +7,70 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import geoTeamIPI.GeoPatrimoine.entity.Place;
 import geoTeamIPI.GeoPatrimoine.entity.Story;
-import geoTeamIPI.GeoPatrimoine.entity.Type;
-import geoTeamIPI.GeoPatrimoine.entity.User;
 import geoTeamIPI.GeoPatrimoine.repository.StoryRepository;
-import org.springframework.data.domain.Sort;
 
 @Service
 public class StoryService {
-		@Autowired
-		private StoryRepository storyRepository;
-		
-		public Story findById(Long id) {
-			return storyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Failed to get a story with the id=" + id +"!!!"));
-		}
-		
-		/**
-		public Story findByCreatorUser(User creatorUser) {
-			return storyRepository.findBycreatorUser(creatorUser); 
-		}
-		*/
-		
-		/**
-		public Story findByPlace(Place place) {
-			return storyRepository.findByPlace(place); 
-		}
-		
-		public Story findByType(Type type) {
-			return storyRepository.findByType(type); 
-		}
-		*/
-		
-		public Story findByDateCreation(LocalDate date_creation) {
-			return storyRepository.findByDateCreation(date_creation); 
-		}
-		
-		public Story findByDateUpdate(LocalDate dateUpdate) {
-			return storyRepository.findByDateUpdate(dateUpdate); 
-		}
-		
-		public Long countAllStorys() {
-			return storyRepository.count();
-		}
-		
-		public Page<Story> findAllStories(Integer page, Integer size, String sortProperty, String sortDirection){
-	        @SuppressWarnings("deprecation")
-			Sort sort = new Sort(new Sort.Order(Sort.Direction.fromString(sortDirection),sortProperty));
-	        @SuppressWarnings("deprecation")
-			Pageable pageable = new PageRequest(page,size,sort);
-			return storyRepository.findAll(pageable); 
-		}
-		
-		public List<Story> findAllStories(){
-			return storyRepository.findAll(); 
-		}
-		
-		public Story createStory(Story story) {
-			return storyRepository.save(story); 
-		} 
-		
-		public <T extends Story> T updateStory(Long id, T story) {
-			story.setId(id);
-			return storyRepository.save(story); 
-		}
-		
-		public void deleteStory(Story story) {
-			storyRepository.delete(story);
-		}
-			
+	@Autowired
+	private StoryRepository storyRepository;
+
+	public Long countAllStories() {
+		return storyRepository.count();
 	}
 
+	public List<Story> findAllStories() {
+		return storyRepository.findAll();
+	}
+
+	public Page<Story> findAllStories(Integer page, Integer size, String sortProperty, String sortDirection) {
+		@SuppressWarnings("deprecation")
+		Sort sort = new Sort(new Sort.Order(Sort.Direction.fromString(sortDirection), sortProperty));
+		@SuppressWarnings("deprecation")
+		Pageable pageable = new PageRequest(page, size, sort);
+		return storyRepository.findAll(pageable);
+	}
+
+	public Story createStory(Story story) {
+		LocalDate todaysDate = LocalDate.now();
+		story.setDateCreation(todaysDate);
+		return storyRepository.save(story);
+	}
+
+	public Story findById(Long id) {
+		return storyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Failed to get a story with the id=" + id + "!!!"));
+	}
+
+	public void deleteStory(Story story) {
+		storyRepository.delete(story);
+	}
+
+	public <T extends Story> T updateStory(Long id, T story) {
+		story.setId(id);
+		return storyRepository.save(story);
+	}
+
+	// --------------------------------------------A VERIFIER TOUT CE QUI EST EN DESSOUS
+
+	/**
+	 * public Story findByCreatorUser(User creatorUser) { return storyRepository.findBycreatorUser(creatorUser); }
+	 */
+
+	/**
+	 * public Story findByPlace(Place place) { return storyRepository.findByPlace(place); }
+	 * 
+	 * public Story findByType(Type type) { return storyRepository.findByType(type); }
+	 */
+
+	public Story findByDateCreation(LocalDate date_creation) {
+		return storyRepository.findByDateCreation(date_creation);
+	}
+
+	public Story findByDateUpdate(LocalDate dateUpdate) {
+		return storyRepository.findByDateUpdate(dateUpdate);
+	}
+
+}

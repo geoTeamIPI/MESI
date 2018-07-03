9f1733a82593f6291426f456f7f11ebbf7ee3533
Pcharlet <charletpierre6@gmail.com>
2018-07-03 22:27:06 +0200

    ajout findbyUser

--- back/src/main/java/geoTeamIPI/GeoPatrimoine/repository/StoryRepository.java
+++ back/src/main/java/geoTeamIPI/GeoPatrimoine/repository/StoryRepository.java
@@ -1,13 +1,18 @@
 package geoTeamIPI.GeoPatrimoine.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import geoTeamIPI.GeoPatrimoine.entity.Story;
+import geoTeamIPI.GeoPatrimoine.entity.User;
 
 @Repository
 public interface StoryRepository extends JpaRepository<Story, Long> {
-	Optional<Story> findById(Long id);
+	
+	List<Story> findByUser(User user);
+	
+	
 	}
 

df54cb1281a4dffc53ff08c31a8b4c81f00cac0e
Pcharlet <charletpierre6@gmail.com>
2018-07-03 22:30:20 +0200

    ajout fidnbyTimelapse

--- back/src/main/java/geoTeamIPI/GeoPatrimoine/repository/StoryRepository.java
+++ back/src/main/java/geoTeamIPI/GeoPatrimoine/repository/StoryRepository.java
@@ -1,11 +1,13 @@
 package geoTeamIPI.GeoPatrimoine.repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import geoTeamIPI.GeoPatrimoine.entity.Story;
+import geoTeamIPI.GeoPatrimoine.entity.Timelapse;
 import geoTeamIPI.GeoPatrimoine.entity.Type;
 import geoTeamIPI.GeoPatrimoine.entity.User;
 
@@ -15,6 +17,12 @@ public interface StoryRepository extends JpaRepository<Story, Long> {
 	List<Story> findByUser(User user);
 	
 	Story findByType(Type type);
+	
+	Story findbyTimelapse (Timelapse timelapse);
+	
+	
+	
+	
 	
 	}
 

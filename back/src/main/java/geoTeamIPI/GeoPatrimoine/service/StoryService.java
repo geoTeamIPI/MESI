19f091ed747a5022ec4a6b9ab491d03febe86ad8
rousslud <rousselot.ludovic@yahoo.fr>
2018-07-05 11:49:53 +0200

    [BACK][CLEANING] Cleaning useless functions

--- back/src/main/java/geoTeamIPI/GeoPatrimoine/service/StoryService.java
+++ back/src/main/java/geoTeamIPI/GeoPatrimoine/service/StoryService.java
@@ -5,10 +5,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import geoTeamIPI.GeoPatrimoine.entity.Story;
@@ -24,11 +20,15 @@ public class StoryService {
 	@Autowired
 	private UserRepository userRepository;
 
+	public Story findById(Long id) {
+		return storyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Failed to get a story with the id=" + id + "!!!"));
+	}
+
 	public Long countAllStories() {
 		return storyRepository.count();
 	}
 
-	public int countAllStoriesOfMyself(Long idUser) {
+	public int countAllStories(Long idUser) {
 		Optional<User> user = userRepository.findById(idUser);
 		List<Story> result = storyRepository.findByCreator(user);
 		return result.size();
@@ -38,44 +38,9 @@ public class StoryService {
 		return storyRepository.findAll();
 	}
 
-	public Page<Story> findAllStories(Integer page, Integer size, String sortProperty, String sortDirection) {
-		@SuppressWarnings("deprecation")
-		Sort sort = new Sort(new Sort.Order(Sort.Direction.fromString(sortDirection), sortProperty));
-		@SuppressWarnings("deprecation")
-		Pageable pageable = new PageRequest(page, size, sort);
-		return storyRepository.findAll(pageable);
-	}
-
-	public List<Story> findAllStoriesOfUser(User user) {
-		return storyRepository.findByCreator(user);
-	}
-
-	public Page<Story> findAllStoriesOfUser(User user, Integer page, Integer size, String sortProperty, String sortDirection) {
-		@SuppressWarnings("deprecation")
-		Sort sort = new Sort(new Sort.Order(Sort.Direction.fromString(sortDirection), sortProperty));
-		@SuppressWarnings("deprecation")
-		Pageable pageable = new PageRequest(page, size, sort);
-		return storyRepository.findAllByCreator(user, pageable);
-	}
-
-	public List<Story> findAllStoriesOfMyself(Long idUser) {
+	public List<Story> findAllStories(Long idUser) {
 		Optional<User> user = userRepository.findById(idUser);
 		return storyRepository.findByCreator(user);
-	}
-
-	public Page<Story> findAllStoriesOfMyself(Long idUser, Integer page, Integer size, String sortProperty, String sortDirection) {
-		@SuppressWarnings("deprecation")
-		Optional<User> connectedUser = userRepository.findById(idUser);
-		Sort sort = new Sort(new Sort.Order(Sort.Direction.fromString(sortDirection), sortProperty));
-		@SuppressWarnings("deprecation")
-		Pageable pageable = new PageRequest(page, size, sort);
-		return storyRepository.findAllByCreator(connectedUser, pageable);
-	}
-
-	public Story createStory(Story story) {
-		LocalDate todaysDate = LocalDate.now();
-		story.setDateCreation(todaysDate);
-		return storyRepository.save(story);
 	}
 
 	public Story createStory(Story story, User user) {
@@ -83,14 +48,6 @@ public class StoryService {
 		story.setCreator(user);
 		story.setDateCreation(todaysDate);
 		return storyRepository.save(story);
-	}
-
-	public Story findById(Long id) {
-		return storyRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Failed to get a story with the id=" + id + "!!!"));
-	}
-
-	public void deleteStory(Story story) {
-		storyRepository.delete(story);
 	}
 
 	public void deleteStory(Story story, User user) {
@@ -101,11 +58,6 @@ public class StoryService {
 		} else {
 			// throw denied
 		}
-	}
-
-	public <T extends Story> T updateStory(Long id, T story) {
-		story.setId(id);
-		return storyRepository.save(story);
 	}
 
 	public <T extends Story> void updateStory(Long id, Story story, User user) {
@@ -135,12 +87,12 @@ public class StoryService {
 	 * public Story findByType(Type type) { return storyRepository.findByType(type); }
 	 */
 
-	public Story findByDateCreation(LocalDate date_creation) {
-		return storyRepository.findByDateCreation(date_creation);
-	}
+	/**
+	 * public Story findByDateCreation(LocalDate date_creation) { return storyRepository.findByDateCreation(date_creation); }
+	 */
 
-	public Story findByDateUpdate(LocalDate dateUpdate) {
-		return storyRepository.findByDateUpdate(dateUpdate);
-	}
+	/**
+	 * public Story findByDateUpdate(LocalDate dateUpdate) { return storyRepository.findByDateUpdate(dateUpdate); }
+	 */
 
 }

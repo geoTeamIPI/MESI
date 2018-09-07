56ba7ccd13225c63fb4c7311fc18d2bcf6062d85
cedricBerth <cedric.berthezene@cgi.com>
2018-09-07 14:41:54 +0200

    [FRONT] Intermediary commit

--- front/projects/showcase/src/app/story/add-story/add-story.component.ts
+++ front/projects/showcase/src/app/story/add-story/add-story.component.ts
@@ -5,31 +5,28 @@ import { Story } from '../story.model';
 import { StoryLocation } from '../story-location.model';
 import { StoryService } from '../story.service';
 
-
 @Component({
   templateUrl: './add-story.component.html'
 })
 export class AddStoryComponent {
 
+  submitted : boolean; 
   story: Story = new Story();
-  storyLocation : StoryLocation = new StoryLocation();
+  storyLocation: StoryLocation = new StoryLocation(); 
 
-  constructor( private storyService: StoryService, route: ActivatedRoute) {
-    route.queryParams.subscribe(params => {
-      this.storyLocation.lat = params['lat'];
-      this.storyLocation.lng = params['lng'];
-      
-  });
-   
+  constructor( private storyService: StoryService, route: ActivatedRoute) {}
+
+  onSubmit(){
+    this.createStory(); 
+    this.submitted = true;
   }
 
   createStory(): void {
-    
-    this.story.storyLocation = this.storyLocation;
     console.log(this.story);
     this.storyService.createStory(this.story)
         .subscribe( data => {
-          alert("Story created successfully.");
+          console.log("Création de l'histoire");
+        }, error => {
         });
 
   };

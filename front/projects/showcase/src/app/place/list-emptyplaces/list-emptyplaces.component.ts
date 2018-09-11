95da30b3fca38debc448eea73ace9de341cb555b
rousslud <rousselot.ludovic@yahoo.fr>
2018-09-11 11:21:15 +0200

    [ALZHEIMER] Don't remember what I did

--- front/projects/showcase/src/app/place/list-emptyplaces/list-emptyplaces.component.ts
+++ front/projects/showcase/src/app/place/list-emptyplaces/list-emptyplaces.component.ts
@@ -10,7 +10,7 @@ import { User } from "../../models/user.model";
   templateUrl: "./list-emptyplaces.component.html",
   styleUrls: ["./list-emptyplaces.component.css"]
 })
-
+ 
 export class ListEmptyPlacesComponent implements OnInit {
   places: Place[];
   size: number;
